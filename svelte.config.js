be861998620c2bc1a0f69772e99c470f7ebc5738
Guest User <Guest@michaellangs-MacBook-Pro.local>
2026-04-13 08:10:07 +0800

    changed base from cas-advance to /

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
   },    
     adapter: adapter({ fallback: '404.html' }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/' : ''
+      base: process.env.NODE_ENV === 'production' ? '' : ''
     }
   }
 };
